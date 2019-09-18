5132a1e4f84259e5d1589fa46c6a346bc2f4c1b9
aglegaspi <lex83nyc@gmail.com>
2019-09-18 12:52:35 -0400

    first commit

---
+++ Swift_Unit3_Review/View Controllers/PodcastDetailViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class PodcastDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Swift_Unit3_Review/View Controllers/PodcastViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class PodcastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
